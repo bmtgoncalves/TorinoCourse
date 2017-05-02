065abfee0358a956e8c066f1a9661031667c9dc4
Bruno Gonçalves <bgoncalves@gmail.com>
2017-05-02 17:27:13 +0200

    Lecture IV Slides

--- Lecture IV/google_place_search.py
+++ Lecture IV/google_place_search.py
@@ -9,10 +9,10 @@ geocode_result = gmaps.places("pizza", (45.090219, 7.659144), 10000)
 result = geocode_result["results"][0]
 print(result["formatted_address"])
 
-time.sleep(1)
+time.sleep(5)
 page_token = geocode_result["next_page_token"]
 geocode_result = gmaps.places(None, page_token=page_token)
 result = geocode_result["results"][0]
 
-pprint(result)
+print(result["formated_address"])
 
